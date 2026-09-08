62df40917907cb095a86ef30a94f3cbfb14e7e70
Fenlir <l14.92@hotmail.com>
2026-09-07 22:50:24 -0600

    ejercicios terminados

--- Intermediate Python/decorators/decorators_u.py
+++ Intermediate Python/decorators/decorators_u.py
@@ -1,9 +1,9 @@
 from datetime import date
 
-class user():
-    def __init__(self,date_of_birth):
-        self.date_of_birth = date_of_birth
 
+class User:
+    def __init__(self, date_of_birth):
+        self.date_of_birth = date_of_birth
 
     @property
     def age(self):
@@ -19,25 +19,27 @@ class user():
 
         return age
 
+
 def check_age(func):
     def wrapper(*args, **kwargs):
 
-        user = args[0]
+        current_user = args[0]
 
-        if user.age < 18:
+        if current_user.age < 18:
             raise ValueError("El usuario debe ser mayor de edad")
 
         return func(*args, **kwargs)
 
     return wrapper
 
+
 @check_age
 def legal_age(user):
     print("mayor de edad")
 
-fenlir = user(date(1999, 5, 20))
 
-rilenf = user(date(2020, 5, 20))
+fenlir = User(date(1999, 5, 20))
+rilenf = User(date(2020, 5, 20))
 
 legal_age(fenlir)
 
